c0c09f6de7fe6f5dccbdf5730c40f5a38e78b4f1
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-07 17:38:33 +0900

    feat: argon2를 사용한 비밀번호 검증 로직 구현
    NestJs 공식 홈페이지에서 암호화 툴로 bcrypt와 argon2를 추천하여
    두 툴을 비교한 결과 bcrypt는 오랜 기간 사용되었고 argon2는 가장 안전한 보안 기술 중 하나였기 때문에
    사용자 비밀번호 암호화에는 argon2가 적합하다고 판단
    argon2를 사용해 비밀번호를 검증하는 verify 메서드를 통해 boolean 값의 검증 유무를 판단하여
    false인 경우 예외 발생

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
 import { LoginReqDto } from '../dto/login.req';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -9,13 +10,24 @@ export class AuthService {
   async login(reqDto: LoginReqDto) {
     const { email, rawPassword } = reqDto;
 
-    await this.validateUser(email);
+    await this.validateUser(email, rawPassword);
   }
 
-  private async validateUser(email: string) {
+  private async validateUser(email: string, rawPassword: string) {
     const user = await this.userRepository.findOneByEmail(email);
     if (!user) {
       throw new BadRequestException('사용자를 찾을 수 없습니다.');
     }
+
+    if (!this.isVerifyPassword(rawPassword, user.encrypedPassword)) {
+      throw new BadRequestException('비밀번호가 일치하지 않습니다.');
+    }
+  }
+
+  private async isVerifyPassword(
+    rawPassword: string,
+    encryptedPassword: string,
+  ): Promise<boolean> {
+    return await argon2.verify(rawPassword, encryptedPassword);
   }
 }
