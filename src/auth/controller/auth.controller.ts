453df531ea60013f6a9ed8653e0e851ea74c471a
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-07 20:31:38 +0900

    feat: 로그인 controller 구현
    로그인 로직을 가지고 있는 AuthService 의존성 주입
    Get/ auth/login api
    Login data를 받아 상태코드 200과 함께 반환

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { CreateUserReqDto } from '../dto/create-user.req';
 import { ResponseDto } from 'src/utils/dto/response.dto';
+import { LoginReqDto } from '../dto/login.req';
+import { LoginResDto } from '../dto/login.res';
+import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('signup')
   async signupUser(
@@ -14,5 +20,14 @@ export class AuthController {
     await this.userService.createUser(createUserDto);
     const result = new ResponseDto(201);
     return result;
+  }
+
+  @Get('login')
+  async login(
+    @Body() loginDto: LoginReqDto,
+  ): Promise<ResponseDto<LoginResDto>> {
+    const data = await this.authService.login(loginDto);
+    const response = new ResponseDto(200, null, data);
+    return response;
   }
 }
