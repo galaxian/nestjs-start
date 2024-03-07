3bf1fa5a3964eab6a8dabc435dd8708a4c007879
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-07 23:53:27 +0900

    feat: typeorm config 파일 위치 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'config/typeorm.config';
+import { typeORMConfig } from 'src/config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
