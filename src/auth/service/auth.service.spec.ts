ea7f53cd61022bb052bccd849502ce6d8a00600e
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-08 00:02:48 +0900

    test: authService 로그인 성공 테스트 코드 작성
    jwt, config, argon2, userRepo 등 AuthService 외부의 영향을 받지 않도록
    mocking을 활용해 테스트 의존성을 감소

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -3,6 +3,11 @@ import { UserRepository } from '../repository/user.repository';
 import { AuthService } from './auth.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { JwtService } from '@nestjs/jwt';
+import { LoginReqDto } from '../dto/login.req';
+import { User } from '../entity/user.entity';
+import { LoginResDto } from '../dto/login.res';
+import * as config from 'config';
+import * as argon2 from 'argon2';
 
 class MockUserRepository {
   findOneByEmail(email: string) {
@@ -12,7 +17,7 @@ class MockUserRepository {
 
 class MockJwtService {
   sign(payload: TokenPayload, option: { expiresIn: number; secret: string }) {
-    return Promise.resolve();
+    return Promise.resolve(null);
   }
 }
 
@@ -33,5 +38,62 @@ describe('authService', () => {
     authService = module.get<AuthService>(AuthService);
     jwtService = module.get<JwtService>(JwtService);
     userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('로그인 성공', async () => {
+      //given
+      const findUser: User = {
+        id: 'uuid',
+        email: 'test@example.com',
+        encrypedPassword: 'password123',
+        phone: null,
+        createdAt: null,
+        lastModifiedAt: null,
+      };
+
+      const loginReqDto: LoginReqDto = {
+        email: 'test@example.com',
+        rawPassword: 'password123',
+      };
+
+      const findOneByEmailSpy = jest
+        .spyOn(userRepository, 'findOneByEmail')
+        .mockResolvedValueOnce(findUser);
+
+      const mockConfigValue = {
+        accessSecret: 'mockAccessSecret',
+        accessExpire: 1000,
+        refreshSecret: 'mockRefreshSecret',
+        refreshexpire: 3000,
+      };
+
+      const mockConfig = jest
+        .spyOn(config, 'get')
+        .mockReturnValue(JSON.stringify(mockConfigValue));
+
+      const mockSign = jest
+        .spyOn(jwtService, 'sign')
+        .mockReturnValueOnce('access-token')
+        .mockReturnValueOnce('refresh-token');
+
+      const mockArgon = jest
+        .spyOn(argon2, 'verify')
+        .mockResolvedValueOnce(true);
+
+      //when
+      const result: LoginResDto = await authService.login(loginReqDto);
+
+      //then
+      expect(result.accessToken).toEqual('access-token');
+      expect(result.refreshToken).toEqual('refresh-token');
+
+      expect(mockConfig).toHaveBeenCalledTimes(2);
+      expect(mockSign).toHaveBeenCalledTimes(2);
+    });
   });
 });
