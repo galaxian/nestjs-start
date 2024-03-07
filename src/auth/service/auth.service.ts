018e41c2b331b950ef5805594b47f091c2a1e606
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-07 17:29:57 +0900

    feat: 이메일을 통한 유저 검증 로직 구현
    이메일을 사용해 DB를 조회했을 때 조회된 유저가 없으면
    email을 잘못 입력했거나 회원가입 하지 않은 유저기 때문에
    예외 발생

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+import { LoginReqDto } from '../dto/login.req';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async login(reqDto: LoginReqDto) {
+    const { email, rawPassword } = reqDto;
+
+    await this.validateUser(email);
+  }
+
+  private async validateUser(email: string) {
+    const user = await this.userRepository.findOneByEmail(email);
+    if (!user) {
+      throw new BadRequestException('사용자를 찾을 수 없습니다.');
+    }
+  }
+}
