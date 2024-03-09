b22d83a382ce60f004418d8577914f3ead2ab3f8
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-10 00:06:48 +0900

    feat: typeorm 설정 app.module.ts로 변경 및 configModule 적용
    typeorm.config.ts의 설정정보를 app.module.ts에서 설정하도록 변경했다.
    이후 ConfigModule을 사용해 typeorm 설정정보를 환경변수에서 가져 오는데 성공
    autoLoadEntities 옵션을 사용해 entity를 찾는 방식을 변경했다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'src/config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<number>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        synchronize: Boolean(configService.get<boolean>('DB_SYNCHRONIZE')),
+        autoLoadEntities: true,
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
