f4f37ee373142252c5266f7c1ba168c0d3c2658d
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-12 18:04:04 +0900

    test: 상품 등록 성공 테스트 코드
    테스트 중 typeorm-transaction 라이브러리의 @Transactional() 데코레이터에 의해
    datasource가 없어 테스트가 통과하지 않는 문제가 발생했다.
    typeorm-transaction 라이브러리 공식 문서 확인 결과
    mocking 방법에 대해 알려주고 있어 문서 그대로 사용했다.

--- src/product/service/product.service.spec.ts
+++ src/product/service/product.service.spec.ts
@@ -9,6 +9,10 @@ class MockProductRepository {
   }
 }
 
+jest.mock('typeorm-transactional', () => ({
+  Transactional: () => () => ({}),
+}));
+
 describe('ProductService', () => {
   let productService: ProductService;
   let productRepository: ProductRepository;
@@ -27,5 +31,29 @@ describe('ProductService', () => {
 
   it('should be defined', () => {
     expect(productService).toBeDefined();
+  });
+
+  describe('createProduct', () => {
+    it('상품 등록 성공', async () => {
+      //given
+      const reqDto = {
+        name: '태블릿',
+        price: 100000,
+        stock: 10,
+        imageUrl: 'https://~~~',
+        description: '매우 가성비 좋은 태블릿',
+        category: '전자기기',
+      };
+
+      const createProductSpy = jest
+        .spyOn(productRepository, 'createProduct')
+        .mockResolvedValueOnce(null);
+
+      //when
+      await productService.createProduct(reqDto);
+
+      //then
+      expect(createProductSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
