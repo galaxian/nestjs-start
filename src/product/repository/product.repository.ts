a1d61de6b8e41eb526f6ee0b8ee4327e0ab3fdb9
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-12 16:52:06 +0900

    feat: productRepository 생성

---
+++ src/product/repository/product.repository.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { EntityManager, Repository } from 'typeorm';
+import { Product } from '../entities/product.entity';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProductRepository extends Repository<Product> {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {
+    super(
+      productRepository.target,
+      productRepository.manager,
+      productRepository.queryRunner,
+    );
+  }
+}
