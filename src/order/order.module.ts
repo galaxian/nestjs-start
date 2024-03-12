37c4480b3ca642e4f140e7c0c9bfd27acb5e7465
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-13 01:53:26 +0900

    feat: 쿠폰 등록 컨트롤러

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coupon } from './entities/coupon.entity';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { CouponService } from './service/coupon.service';
+import { CouponRepository } from './repository/coupon.repository';
+import { CouponController } from './controller/coupon.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Coupon, Order, OrderItem])],
+  providers: [CouponService, CouponRepository],
+  controllers: [CouponController],
+})
 export class OrderModule {}
