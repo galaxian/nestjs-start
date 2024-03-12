1a2ee1a3392a47d7a85eeab6f99688a03a85303b
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-13 02:15:13 +0900

    test: 쿠폰 등록 성공 테스트 코드

--- src/order/service/coupon.service.spec.ts
+++ src/order/service/coupon.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Coupon } from '../entities/coupon.entity';
 import { CouponRepository } from '../repository/coupon.repository';
 import { CouponService } from './coupon.service';
+import { CouponType } from '../entities/coupon-type';
 
 class MockCouponRepository {
   createCoupon(coupon: Coupon) {
@@ -31,5 +32,26 @@ describe('CouponService', () => {
 
   it('should be defined', () => {
     expect(couponService).toBeDefined();
+  });
+
+  describe('createCoupon', () => {
+    it('쿠폰 등록 성공', async () => {
+      //given
+      const type: CouponType = 'fixed';
+      const reqDto = {
+        couponType: type,
+        value: 5,
+        validFrom: '2024-03-13 20',
+        validUntil: '2024-03-20 20',
+      };
+
+      jest.spyOn(couponRepository, 'createCoupon').mockResolvedValueOnce(null);
+
+      //when
+      await couponService.createCoupon(reqDto);
+
+      //then
+      expect(couponRepository.createCoupon).toHaveBeenCalledTimes(1);
+    });
   });
 });
