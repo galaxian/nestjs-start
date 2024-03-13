3a26412113cc0bb619ee6ac0dcd1dc7db25939eb
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-13 18:44:10 +0900

    feat: orderModule 설정

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,10 +6,23 @@ import { OrderItem } from './entities/order-item.entity';
 import { CouponService } from './service/coupon.service';
 import { CouponRepository } from './repository/coupon.repository';
 import { CouponController } from './controller/coupon.controller';
+import { OrderController } from './controller/order.controller';
+import { OrderService } from './service/order.service';
+import { OrderRepository } from './repository/order.repository';
+import { OrderItemRepository } from './repository/order-item.repository';
+import { ProductRepository } from 'src/product/repository/product.repository';
+import { Product } from 'src/product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coupon, Order, OrderItem])],
-  providers: [CouponService, CouponRepository],
-  controllers: [CouponController],
+  imports: [TypeOrmModule.forFeature([Coupon, Order, OrderItem, Product])],
+  providers: [
+    CouponService,
+    CouponRepository,
+    OrderService,
+    OrderRepository,
+    OrderItemRepository,
+    ProductRepository,
+  ],
+  controllers: [CouponController, OrderController],
 })
 export class OrderModule {}
