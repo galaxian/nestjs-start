a56e4094d288cb115311cfe6e4d8c35d97ab6ee1
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-14 20:58:21 +0900

    feat: payment entity 설계
    payment에는 결제에 사용할 값들을 컬럼으로 포함했으며
    결제 과정이 끝나고 필요한 값들은 nullable: false를 주었다

---
+++ src/payment/entities/payment.entity.ts
@@ -0,0 +1,35 @@
+import { User } from 'src/auth/entity/user.entity';
+import { BaseEntity } from 'src/utils/entity/base.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export type PayType = 'card' | 'cash';
+
+@Entity()
+export class Payment extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 25 })
+  payType: PayType;
+
+  @Column({ type: 'int' })
+  amount: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  orderNo: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  orderId: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  paymentKey: string;
+
+  @Column({ type: 'text', nullable: true })
+  failReason: string;
+
+  @Column({ type: 'text', nullable: true })
+  cancelReason: string;
+
+  @ManyToOne(() => User)
+  user: User;
+}
